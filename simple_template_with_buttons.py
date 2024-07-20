1cc5dcbf8881ed0fb6f591b97e8f694fb965cffd
Kai Wilson <Kahswdirect23@gmail.com>
2024-07-20 10:20:56 -0400

    Update simple_template_with_buttons.py

--- simple_template_with_buttons.py
+++ simple_template_with_buttons.py
@@ -19,11 +19,11 @@ theme.cyborg()
 
 with ui.sidebar(id="sidebar_left"):
     ui.tags.link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css")
-    with ui.panel_conditional("input.page == 'Time Series'"):
-        ui.input_radio_buttons("time_radio", "Select Category", {"1":'Number of Complaints', "2":"Review Ratings"})
+    with ui.panel_conditional("input.page == 'Example 1'"):
+        ui.input_radio_buttons("ex1_radio", "Select Category", {"1":'Button 1', "2":"Button 2"})
     
-    with ui.panel_conditional("input.page == 'Natural Language Processor'"):
-        ui.input_radio_buttons("nlp_radio", "Select Category", {"1":"Key word Word Cloud","2": "Review Sentiment Analysis"})
+    with ui.panel_conditional("input.page == 'Example 2'"):
+        ui.input_radio_buttons("ex2_radio", "Select Category", {"1":'Button 1', "2":"Button 2"})
 
 
 
@@ -31,17 +31,17 @@ def nav_content(title, body_id):
     with ui.div(class_="container mx-auto p-4", id = body_id):
         sys.displayhook(ui.h1(title, class_="text-3xl font-bold text-yellow-400 mb-4"))
 
-time_title = "Time Series"
-time_id =  "time"
-with ui.nav_panel("Time Series", ):
+time_title = "Example 1"
+time_id =  "ex1
+with ui.nav_panel("Example 1" ):
     # Body
     nav_content(time_title, time_id)
     
 
 
-nlp_title = "Natural Language Processing"
-nlp_id  =  "nlp"
-with ui.nav_panel("Natural Language Processor"):
+nlp_title = "Example 2"
+nlp_id  =  "ex2"
+with ui.nav_panel("Example 2"):
     nav_content(nlp_title, nlp_id)
     # Side bar
 
